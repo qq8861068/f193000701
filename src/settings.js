e43d4fa9993b6533775a7c416f4c3c7201d6fcd4
qq8861068 <8861068@qq.com>
2024-05-20 12:21:30 +0800

    111

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/StartScene.fire",orientation:"",jsList:["assets/Alibs/base_lib.js"]};
